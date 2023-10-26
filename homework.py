d74e8fe1136f7f78312663fb3b04e23a7f002a87
Екатерина Карпова <karpova.cestmoi@gmail.com>
2023-10-26 13:16:12 +0300

    Homework 6: fix timer

--- homework.py
+++ homework.py
@@ -133,6 +133,7 @@ def main():
     while True:
         try:
             response = get_api_answer(timestamp)
+            timestamp = response.get('current_date')
             homeworks = check_response(response)
             if homeworks:
                 message = parse_status(homeworks[0])
